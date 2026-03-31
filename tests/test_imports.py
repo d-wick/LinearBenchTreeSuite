9216dc2b72de709eb69434e05b929ca19942c855
D. Wick <ch@dwick.me>
2026-03-31 12:27:35 -0400

    Fix pytest test discovery

---
+++ tests/test_imports.py
@@ -0,0 +1,12 @@
+def test_package_import():
+    import linearbenchtree
+
+def test_dataprocessing_import():
+    import linearbenchtree.dataprocessing
+
+def test_data_loader_import():
+    from linearbenchtree.dataprocessing import data_loader_processed
+
+def test_model_imports():
+    import linearbenchtree.decisiontree
+    import linearbenchtree.randomforest
